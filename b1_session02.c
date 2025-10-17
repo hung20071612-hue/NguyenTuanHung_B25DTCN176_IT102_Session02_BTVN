c853c3d9ea4e91e063dc00d4aa4f72166b3c47f2
hung20071612-hue <hung20071612@gmail.com>
2025-10-17 23:09:14 +0700

    Add files via upload

---
+++ b1_session02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	short a = 10 ;//short; so nguyen nho, 2 byte
+	int b = 100 ;//int: so nguyen nho, 4 byte
+	long c = 1000 ;//long: so nguyen, 4 byte 
+	long long d = 10000;//long long : so nguyen, 8 byte 
+	unsigned short e = 10000;//unsigned short: so nguyen, 2 byte
+	unsigned int f = 100000;//unsigned int: so nguyen, 4 byte
+	unsigned long g = 1000000;//unsigned long: so nguyen, 4 byte
+	unsigned long long h = 10000000;//unsigned long long: so nguyen, 8 byte
+	char i = 'a' ;//char:so nguyen, Ky tu, 1 byte, 
+	return 0;
+}
+
